1dde68cc8bbe4d500a186fbbcf7d66ae6060bcd7
Neil Yuen <weaming@users.noreply.github.com>
2016-03-25 17:50:29 +0800

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,44 +1,64 @@
-!function(){
+!function translateGithub(){
     // 汉化模板
     var pages = {};
     function addPage(name) {
         pages[name] = {};
-        pages[name].html = {};
+        pages[name].star = {};
         pages[name].text = {};
+        pages[name].watch = {};
     }
 
-    function addTranslate(page, selector, type, value) {
+    function addTranslate(page, type, selector, value) {
         pages[page][type][selector] = value;
     }
-    // 测试模板
-    addPage('default');
-    addTranslate('default', "body>div.header.header-logged-in.true>div>ul.header-nav.left>li:nth-child(1)>a", "text", "Pull 请求列表");
-    addTranslate('default', "body>div.header.header-logged-in.true>div>ul.header-nav.left>li:nth-child(2)>a", "text", "问题");
-    addPage('test');
 
-    // 获取页面类型
-    function pageType() {
-        return "test";
-    }
+    // 模板
+    addPage('default');
+    addTranslate('default', "text", "body>div.header.header-logged-in.true>div>ul.header-nav.left>li:nth-child(1)>a", "请求列表");
+    addTranslate('default', "text", "body>div.header.header-logged-in.true>div>ul.header-nav.left>li:nth-child(2)>a", "问题列表");
+    addTranslate('default', "text", "div.container a[data-hotkey='g c'] span", "代码");
+    addTranslate('default', "text", "div.container a[data-hotkey='g i'] span[class!='counter']", "问题");
+    addTranslate('default', "text", "div.container a[data-hotkey='g p'] span[class!='counter']", "请求");
+    addTranslate('default', "text", "div.container a[data-hotkey='g w']", "知识");
+
+    addTranslate('default', "star", "ul.pagehead-actions button[aria-label$='tar this repository']", "收藏");
+    addTranslate('default', "star", "ul.pagehead-actions button[aria-label$='tar this repository']", "收藏");
+
+    addTranslate('default', "text", "ul.pagehead-actions span.select-menu-title", "提醒");
+    addTranslate('default', "watch", "ul.pagehead-actions div.select-menu-item-text>input[value='included']", "不关注");
+    addTranslate('default', "watch", "ul.pagehead-actions div.select-menu-item-text>input[value='subscribed']", "关注");
+    addTranslate('default', "watch", "ul.pagehead-actions div.select-menu-item-text>input[value='ignore']", "屏蔽");
 
     function translate(pageType) {
-        translateByType("default");
-        translateByType(pageType);
+        translateByPageType("default");
     }
 
-    function translateByType(type) {
-        var json = pages[type];
-        var html = json.html;
-        for(var selector in html) {
-            $(selector).html(html[selector]);
-        }
+    function translateByPageType(name) {
+        var json = pages[name];
 
         var text = json.text;
         for(var selector in text) {
             $(selector).text(text[selector]);
         }
-    }
 
+        var star = json.star;
+        for(var selector in star){
+            var objs = $(selector);
+            objs[1].innerHTML = objs[1].innerHTML.replace(/Star/g, star[selector]);
+            objs[0].innerHTML = objs[0].innerHTML.replace(/Unstar/g, "取消" + star[selector]);
+        }
+
+        var watch = json.watch;
+        for(var selector in watch){
+            var parent = $(selector).parent();
+            parent.children("span.select-menu-item-heading").text(watch[selector]);
+            // 按钮标题更改未成功
+            // var tmp = parent.children("span.js-select-button-text")[0].innerHTML;
+            // tmp = tmp.replace(/Watch/g, "关注");
+            // tmp = tmp.replace(/Unwatch/g, "取消关注");
+            // tmp = tmp.replace(/Stop ignoring/g, "已屏蔽");
+        }
+    }
     // 实际汉化
-    translate(pageType());
-}();
+    translate('default');
+}()
